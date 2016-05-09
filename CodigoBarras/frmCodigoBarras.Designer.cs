9d01add37d4f1be04493c2c6115f85917f5a4372
Suellen Gularte <sula15@yahoo.com.br>
2016-05-09 11:32:08 -0300

    Backup

---
+++ CodigoBarras/frmCodigoBarras.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CodigoBarras
+{
+    partial class frmCodigoBarras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdtPdf = new System.Windows.Forms.RadioButton();
+            this.rdtDoc = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGerarCodigoBarras = new System.Windows.Forms.Button();
+            this.ofdImportarTxt = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdtPdf
+            // 
+            this.rdtPdf.AutoSize = true;
+            this.rdtPdf.Location = new System.Drawing.Point(18, 25);
+            this.rdtPdf.Name = "rdtPdf";
+            this.rdtPdf.Size = new System.Drawing.Size(66, 24);
+            this.rdtPdf.TabIndex = 0;
+            this.rdtPdf.TabStop = true;
+            this.rdtPdf.Text = "PDF";
+            this.rdtPdf.UseVisualStyleBackColor = true;
+            // 
+            // rdtDoc
+            // 
+            this.rdtDoc.AutoSize = true;
+            this.rdtDoc.Location = new System.Drawing.Point(18, 73);
+            this.rdtDoc.Name = "rdtDoc";
+            this.rdtDoc.Size = new System.Drawing.Size(69, 24);
+            this.rdtDoc.TabIndex = 1;
+            this.rdtDoc.TabStop = true;
+            this.rdtDoc.Text = "DOC";
+            this.rdtDoc.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdtDoc);
+            this.groupBox1.Controls.Add(this.rdtPdf);
+            this.groupBox1.Location = new System.Drawing.Point(495, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Converter para:";
+            // 
+            // btnGerarCodigoBarras
+            // 
+            this.btnGerarCodigoBarras.Location = new System.Drawing.Point(12, 152);
+            this.btnGerarCodigoBarras.Name = "btnGerarCodigoBarras";
+            this.btnGerarCodigoBarras.Size = new System.Drawing.Size(586, 59);
+            this.btnGerarCodigoBarras.TabIndex = 3;
+            this.btnGerarCodigoBarras.Text = "Gerar Código de Barras";
+            this.btnGerarCodigoBarras.UseVisualStyleBackColor = true;
+            this.btnGerarCodigoBarras.Click += new System.EventHandler(this.btnGerarCodigoBarras_Click);
+            // 
+            // ofdImportarTxt
+            // 
+            this.ofdImportarTxt.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(652, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Verificar selecionado (teste)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(652, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // frmCodigoBarras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 250);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGerarCodigoBarras);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmCodigoBarras";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdtPdf;
+        private System.Windows.Forms.RadioButton rdtDoc;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGerarCodigoBarras;
+        private System.Windows.Forms.OpenFileDialog ofdImportarTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CodigoBarras/frmCodigoBarras.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.IO;
+using System.Drawing.Text;
+using System.Web;
+using Root.Reports;
+
+namespace CodigoBarras
+{
+    public partial class frmCodigoBarras : Form
+    {
+        public frmCodigoBarras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerarCodigoBarras_Click(object sender, EventArgs e)
+        {
+            this.ofdImportarTxt.Multiselect = false;
+            this.ofdImportarTxt.Title = "Selecionar Arquivo";
+            ofdImportarTxt.InitialDirectory = @"C:\";
+            ofdImportarTxt.Filter = "Teste | *.txt";
+            ofdImportarTxt.CheckFileExists = true;
+            ofdImportarTxt.CheckPathExists = true;
+            ofdImportarTxt.FilterIndex = 2;
+            ofdImportarTxt.RestoreDirectory = true;
+            ofdImportarTxt.ReadOnlyChecked = true;
+            ofdImportarTxt.ShowReadOnly = true;
+
+            DialogResult dr = this.ofdImportarTxt.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Word.Application wordApplication = default(Microsoft.Office.Interop.Word.Application);
+                wordApplication = new Microsoft.Office.Interop.Word.Application();
+
+                //cria um novo domento Workd
+                object fileName = @"C:\Users\Suellen\Documents\Exemplos_CodigoBarras\Fonts\normal.dot";
+
+                // template normal
+                object newTemplate = false;
+                object docType = 0;
+                object isVisible = true;
+
+                // Cria um novo Documento  chamando a função Add na coleção de documentos
+                Microsoft.Office.Interop.Word.Document aDoc = wordApplication.Documents.Add(Type.Missing, newTemplate, docType, isVisible);// (fileName, newTemplate, docType, isVisible);
+
+                // torna o documento visivel
+                wordApplication.Visible = true;
+                aDoc.Activate();
+
+                StreamReader sr = new StreamReader(ofdImportarTxt.FileName);
+                string linha = sr.ReadLine();
+
+                PrivateFontCollection pfc = new PrivateFontCollection();
+                System.Drawing.Font _Fonte;
+
+                string CAMINHO_FONTES = @"C:\Users\Suellen\Documents\Exemplos_CodigoBarras\Fonts";
+
+                pfc.AddFontFile(CAMINHO_FONTES + @"\" + "BARCOD39.TTF");
+
+                FontFamily fontFamily = pfc.Families[0];
+                _Fonte = new System.Drawing.Font(fontFamily, 30);
+
+                String codigoBarras;
+
+                
+
+                while (linha != null)
+                {
+                    codigoBarras = String.Format("*{0}*", linha);
+
+                    wordApplication.Selection.Font.Size = 80;
+                    wordApplication.Selection.Font.Bold = 0;
+                    wordApplication.Selection.Font.Name = "C39Hrp24dhtt";
+                    wordApplication.Selection.Font.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineNone;
+                    wordApplication.Selection.TypeParagraph();
+                    wordApplication.Selection.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphLeft;
+                    wordApplication.Selection.TypeText(codigoBarras);
+
+                    
+                    linha = sr.ReadLine();
+                }
+
+                sr.Close();
+
+
+                //string vArq = "";
+                //FolderBrowserDialog vSalvar = new FolderBrowserDialog();
+
+                //if (vSalvar.ShowDialog() == DialogResult.Cancel)
+                //    return; // Cancela todo processo
+                // Insere na variavel o caminho selecionado pelo usuário e concatena com o nome do arquivo
+                //vArq = vSalvar.SelectedPath + "\\" + "teste" + ".pdf";
+
+
+                // Cria um objeto PDF
+                //Report vPdf = new Report(new PdfFormatter());
+                // Define a fonte que sera usada no relatório PDF
+                //FontDef vDef = new FontDef(vPdf, FontDef.StandardFont.TimesRoman);
+                //FontProp vDrop = new FontProp(vDef, 10);
+                // Cria uma Nova Pagina
+                //Page vPage = new Page(vPdf);
+
+                // Escreve no Arquivo
+                //vPage.AddCB_MM(5, new RepString(vDrop, codigoBarras)); // Centraliza
+                //vPage.AddCB_MM(0, new RepString(vDrop, ""));
+
+                // Salvar Arquivo no disco
+                //vPdf.Save(vArq);
+                //MessageBox.Show("Arquivo Gerado com sucesso !", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rdtDoc.Checked)
+                textBox1.Text = rdtDoc.Checked.ToString();
+
+            if (rdtPdf.Checked)
+                textBox1.Text = rdtPdf.Checked.ToString();
+        }
+    }
+}
